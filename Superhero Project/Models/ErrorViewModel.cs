fdec87db8c1689399376f95d7966cfc72c24625c
DonaldZheng <dzheng112@gmail.com>
2021-03-16 14:23:38 -0500

    test

---
+++ Superhero Project/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Superhero_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Superhero Project/Models/Superhero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero_Project.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int ID
+        {
+            get; set;
+        }
+        public string SuperheroName
+        {
+            get; set;
+        }
+        public string AlterEgoName
+        {
+        
+            get; set;
+        }
+        public string PrimaryAbility
+        {
+            get; set;
+        }
+        public string SecondaryAbility
+        {
+            get; set;
+        }
+        public string CatchPhrase
+        {
+            get; set;
+        }
+
+
+
+
+    }
+}
